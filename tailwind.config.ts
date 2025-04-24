b2ecbfea05ec5b1d1cb903c1da924c57328ee90f
unknown <ionova.io@outlook.com>
2025-04-24 08:02:07 -0700

    First Commit - HeroSection

---
+++ tailwind.config.ts
@@ -0,0 +1,39 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: "var(--font-sans)",
+      secondary: "var(--font-serif)",
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      },
+    },
+    extend: {
+      colors: {
+        light: "#f4f4f4",
+        primary: "#f4f4f4",
+        accent: "#127bcb",
+        magenta: "#e91e63",
+        dark: "#1e3d58",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
